c84b52f50cc44d572f69e23d9fc5b07e2244c8a5
Bui Chi Trung <trungcspntl@gmail.com>
2022-05-02 19:18:19 +0700

    added

---
+++ container/webpack.config.js
@@ -0,0 +1,25 @@
+
+   
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ModuleFederationPlugin = 
+            require('webpack/lib/container/ModuleFederationPlugin');
+
+module.exports = {
+  mode: 'development',
+  devServer: {
+    port: 8081,
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'container',
+      remotes: {
+        microFrontEnd1: 'microFrontEnd1@http://localhost:8082/remoteEntry.js',
+        microFrontEnd2: 'microFrontEnd2@http://localhost:8083/remoteEntry.js',
+        reactMicroFrontEnd3: 'reactMicroFrontEnd3@http://localhost:8084/remoteEntry.js'
+      },
+    }),
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+};
